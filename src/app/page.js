a4f465cd27bbc1cf4d30ec4a083682ee9dc3cb87
amirnori09 <amirnori795@gmail.com>
2024-02-26 09:52:07 +0330

    building main layout for dashboard

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
-import Navbar from "../components/layouts/Navbar";
+// import Navbar from "../components/layouts/Navbar";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="w-full flex">
+      <div className="bg-red-700 w-2/6 h-screen p-10"></div>
+      <div className="bg-green-600 w-4/6 h-screen"></div>
       
     </div>
   );

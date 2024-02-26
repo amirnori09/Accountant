6b66202ecbaabc7ab83ff206bc139376b8be0e6f
amirnori09 <amirnori795@gmail.com>
2024-02-26 10:24:39 +0330

    add login folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 // import Navbar from "../components/layouts/Navbar";
 
+import SideBar from "@/components/layouts/SideBa";
+
 export default function Home() {
   return (
-    <div className="w-full flex">
-      <div className="bg-red-700 w-2/6 h-screen p-10"></div>
+    <div className=" flex">
+     <SideBar/>
       <div className="bg-green-600 w-4/6 h-screen"></div>
       
     </div>

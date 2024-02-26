588b87cf39a598c5b21d761b73e0e6bbecb518dd
hadisekeykha@gmail.com <hadisekeykha@gmail.com>
2024-02-26 17:15:01 +0330

    login_page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,6 +1,22 @@
 const page = () => {
     return ( 
         <>
+        
+    
+        <div className="container flex items-center justify-center font-sans my-52">
+            <div className="flex flex-col justify-center items-start w-72">
+                <img src="" alt="" />
+                <p className="text-3xl font-black mb-8">خوش آمدید</p>
+                <p className="text-xl mb-7 font-semibold">ورود به حساب کاربری</p>
+                <form action="" className="flex flex-col flex-grow">
+                    <p className="text-gray-700 mb-5 text-lg">شماره موبایل</p>
+                    <input className="w-72 px-3 py-3 rounded-full bg-gray-50 focus:outline-none text-sm" type="text" placeholder="شماره تلفن خود را وارد کنید"/>
+                </form>
+                <button className="bg-[#003928] text-white rounded-full py-4 w-full mt-7">ورود</button>
+
+            </div>
+        </div>
+    
         </>
      );
 }
